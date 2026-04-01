46c176a64d8bcaa5b0ed187bf627c6a9c511cf61
Muhammadbobur22 <muhammadbobur313@gmail.com>
2026-04-01 17:57:08 +0500

    asad

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Dashboard from './components/bobur/dashboard/Dashboard'
 
 const App = () => {
   return (
     <div>
-      
+      <Dashboard/>
     </div>
   )
 }
--- src/components/bobur/Bobur.jsx
+++ src/components/bobur/Bobur.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Bobur = () => {
   return (
-    <div>
-      
-    </div>
+   <div>
+    
+   </div>
   )
 }
 
