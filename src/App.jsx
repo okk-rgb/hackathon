077d152323c0f92952faf32162148bcb7fa6c39a
okk-rgb <baydedayevsoxibjon@gmail.com>
2026-04-01 17:38:35 +0500

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Header_dash from './components/asad/Header_dash'
+import Products from './components/abubakr/Products'
+import AddProducts from './components/abubakr/AddProducts'
 
 const App = () => {
   return (
     <div>
-      
+      <Header_dash/>
+      {/* <Products/> */}
+      {/* <AddProducts/> */}
     </div>
   )
 }
--- src/components/asad/Header_dash.jsx
+++ src/components/asad/Header_dash.jsx
@@ -1,19 +1,44 @@
 import React from 'react'
 import himg from '../../assets/logo.png'
 import { IoIosSearch } from 'react-icons/io'
+import './Header_dash.css'
+import { Link } from 'react-router-dom'
+import { CiFolderOn, CiHome } from 'react-icons/ci'
+import { IoPricetagOutline } from 'react-icons/io5'
 
 const Header_dash = () => {
   return (
     <header className='site__header'>
       <div className="container">
         <div className="header__wrapper">
+          <div className="header__logo">\
+
           <img src={himg} alt="" />
+          </div>
 
           <div className="search__bar">
-            <IoIosSearch />
-            <input type="text" placeholder=''/>
+            <div className="header__icon"><IoIosSearch /></div>
+            <input type="text" placeholder='Search...'/>
           </div>
         </div>
+
+        <aside className='sidebar__wrapper'> 
+          <div className="container">
+          <ul>
+            <li>
+              <button><Link><span><CiHome /></span> Dashboard</Link></button>
+            </li>
+            <li>
+              <button><Link><span><IoPricetagOutline /></span> Products</Link></button>
+              
+            </li>
+            <li>
+              <button><Link><span><CiFolderOn /></span>Categories</Link></button>
+              
+            </li>
+          </ul>
+          </div>
+        </aside>
       </div>
       
     </header>
